6768fd6b66ff041c29cfa6ecf516fbbb44dbeefd
Vinicius Perdigao <eng.viniciusperdigao@gmail.com>
2022-09-27 19:56:55 -0300

    Primeiro Commit

---
+++ Cap06/6-1. Person.py
@@ -0,0 +1,12 @@
+# Use a dictionary to store information about a person you know.
+# Store their first name, last name, age, and the city in which they live. You
+# should have keys such as first_name, last_name, age, and city. Print each
+# piece of information stored in your dictionary.
+
+person = {'first_name':'João', 'last_name':'Pedro', 'age':20, 'city':'Vitória'}
+
+print(person)
+print(person['age'])
+print(person['first_name'])
+print(person['last_name'])
+print(person['city'])
---
+++ Cap06/favorite _languages.py
@@ -0,0 +1,15 @@
+# favorite_languages = {
+# 'jen': 'python',
+# 'sarah': 'c',
+# 'edward': 'ruby',
+# 'phil': 'oi',
+# 'phil': 'test',
+# }
+
+# print(favorite_languages['phil'])
+
+
+
+alien_0 = {'color': 'green', 'speed': 'slow'}
+point_value = alien_0.get('color')
+print(point_value)
