d21740727a990f08a5e73b19328cf592d5f3951f
Vinicius Perdigao <eng.viniciusperdigao@gmail.com>
2022-10-04 18:25:36 -0300

    commit

--- Cap06/favorite _languages.py
+++ Cap06/favorite _languages.py
@@ -10,6 +10,38 @@
 
 
 
-alien_0 = {'color': 'green', 'speed': 'slow'}
-point_value = alien_0.get('color')
-print(point_value)
+# alien_0 = {'color': 'green', 'speed': 'slow'}
+# point_value = alien_0.get('color')
+# print(point_value)
+
+
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+# #RODA OS VALORES NO LOOP
+# for name in favorite_languages.values():
+#     print(name.title())
+
+# #RODA AS CHAVES NO LOOP
+# for name in favorite_languages.keys():
+#     print(name.title())
+
+# #RODA CHAVE E VALOR NO LOOP
+# for name, language in favorite_languages.items():
+#     print(f"{name.title()}'s favorite language is {language.title()}.")
+
+
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(name.title())
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+print("The following languages have been mentioned:")
+for language in sorted(favorite_languages.values()):
+    print(language.title())
